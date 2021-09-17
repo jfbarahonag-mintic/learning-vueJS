d55e59eaea83467bc3991c75bfbd9f2f669f5236
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-09-16 22:56:44 -0500

    some comments were added for a better understanding

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ let app = new Vue({
         title : 'Hello Vue',
         paragraph : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi necessitatibus, aut, accusantium debitis ducimus voluptatibus molestiae dicta omnis iusto, vel distinctio culpa. Similique, perspiciatis vitae quisquam illum delectus incidunt vel.',
         fruits: [
-            {name: "Pear", qty: 10},
-            {name: "Apple", qty: 5},
-            {name: "Grape", qty: 4},
-            {name: "Mango", qty: 0},
+            // {name: "Pear", qty: 10},
+            // {name: "Apple", qty: 5},
+            // {name: "Grape", qty: 4},
+            // {name: "Mango", qty: 0},
         ],
         newFruit: ''
     },
     methods: {
         addFruit() {
+            //ignore empty field
             if(this.newFruit == ""){
                 return
             }
@@ -21,25 +22,30 @@ let app = new Vue({
 
             this.fruits.forEach(fruit => {
                 if (this.newFruit.toUpperCase() == fruit.name.toUpperCase()) {
+                    //ignore listed fruits
                     alert(`This fruit >>${fruit.name}<< was already registered in the list`)
                     exists = true
                 }
             });
 
             if (exists == false) {
+                // start capitalize the name
                 temp = this.newFruit
                 temp = temp.charAt(0).toUpperCase() + temp.slice(1)
+                // end capitalize the name
                 this.fruits.push({
                     name: temp,
+                    // as default must be 0 to avoid undefined values
                     qty: 0
                 })
             }
 
-
+            // clean the field
             this.newFruit = ""
         },
 
         filterText($event) {
+            // 48 = '0'  and 57 = '9' in ASCII code
             if($event.charCode >= 48 && $event.charCode <= 58)
             {
                 //ignore numbers
@@ -54,6 +60,7 @@ let app = new Vue({
         },
         
         decrementQty(fruit) {
+            //ignore negative numbers
             if(fruit.qty == 0) return
             fruit.qty--
         }
